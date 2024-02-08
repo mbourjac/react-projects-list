9ee87d5179c0af6efb77608323115e7267b3f793
Michaël Bourjac <michael.bourjac@gmail.com>
2024-02-08 19:07:59 +0100

    feat: implement fluid typography

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,20 +14,20 @@ export const Home = () => {
             to={slug}
             className={cn(
               'text-off-black hover:bg-primary',
-              !isLastProject(index) && 'mr-4',
+              !isLastProject(index) && 'mr-[calc(0.2rem+0.85vw)]',
             )}
           >
-            <h2 className="inline text-[10.8rem] font-medium leading-none tracking-[-0.5rem]">
+            <h2 className="inline text-[calc(1rem+10.8vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
               {title}
             </h2>
             <span className="inline-flex items-baseline">
               <img
                 src={src}
                 alt=""
-                className="aspect[3/2] ml-4 mr-1 h-[7.5rem]"
+                className="aspect[3/2] ml-[calc(0.2rem+0.85vw)] mr-[calc(0.05rem+0.2vw)] w-[calc(1rem+11.8vw)]"
               />
               {!isLastProject(index) && (
-                <span className="text-[10.8rem] font-medium leading-none tracking-[-0.5rem]">
+                <span className="text-[calc(1rem+10.8vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
                   ,
                 </span>
               )}
