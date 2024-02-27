2b40fc3d9a62847acdd83d3e2a25b10f9af3cf34
Michaël Bourjac <michael.bourjac@gmail.com>
2024-02-27 19:23:17 +0100

    feat: reduce headings font size and images width

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,17 +17,17 @@ export const Home = () => {
               !isLastProject(index) && 'mr-[calc(0.2rem+0.85vw)]',
             )}
           >
-            <h2 className="inline text-[calc(1rem+10.8vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
+            <h2 className="inline text-[calc(1rem+10vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
               {title}
             </h2>
             <span className="inline-flex items-baseline">
               <img
                 src={src}
                 alt=""
-                className="aspect[3/2] ml-[calc(0.2rem+0.85vw)] mr-[calc(0.05rem+0.2vw)] w-[calc(1rem+11.8vw)]"
+                className="aspect[3/2] ml-[calc(0.2rem+0.85vw)] mr-[calc(0.05rem+0.2vw)] w-[calc(1rem+11vw)]"
               />
               {!isLastProject(index) && (
-                <span className="text-[calc(1rem+10.8vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
+                <span className="text-[calc(1rem+10vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
                   ,
                 </span>
               )}
