949fa489805a2b139fbd730c636a88714173ecf0
Michaël Bourjac <michael.bourjac@gmail.com>
2024-04-29 18:42:56 +0200

    feat: rework home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,15 +8,6 @@ export const Home = () => {
 
   return (
     <main className="min-h-screen max-w-full">
-      <Marquee
-        contentCount={2}
-        className="bg-black text-4xl uppercase text-white"
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.
-        </p>
-      </Marquee>
       <div className="px-2 py-2">
         {projectCovers.map(({ id, src, slug, title }, index) => (
           <Link
@@ -27,7 +18,7 @@ export const Home = () => {
               !isLastProject(index) && 'mr-[calc(0.2rem+0.85vw)]',
             )}
           >
-            <h2 className="inline text-[calc(1rem+10vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
+            <h2 className="inline text-[calc(1rem+9vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
               {title}
             </h2>
             <span className="inline-flex items-baseline">
@@ -37,7 +28,7 @@ export const Home = () => {
                 className="aspect[3/2] ml-[calc(0.2rem+0.85vw)] mr-[calc(0.05rem+0.2vw)] w-[calc(1rem+11vw)]"
               />
               {!isLastProject(index) && (
-                <span className="text-[calc(1rem+10vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
+                <span className="text-[calc(1rem+9vw)] font-medium leading-none tracking-[calc(-0.095rem-0.45vw)]">
                   ,
                 </span>
               )}
@@ -45,6 +36,16 @@ export const Home = () => {
           </Link>
         ))}
       </div>
+      <Marquee
+        contentCount={2}
+        duration={105}
+        className="fixed bottom-0 bg-primary text-[calc(1rem+1vw)] text-black"
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.
+        </p>
+      </Marquee>
     </main>
   );
 };
